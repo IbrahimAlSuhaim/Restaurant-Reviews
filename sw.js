05b606de97788054c6099f093a67a98b76eb758c
IbrahimAlSuhaim <ka20@windowslive.com>
2019-06-12 06:47:49 +0300

    feat: add service worker script

---
+++ sw.js
@@ -0,0 +1,23 @@
+const staticCacheName = 'rvw-v0.1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'img/',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
