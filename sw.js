3efd4fd60d938d37a03d14d54712c5e9717ca898
IbrahimAlSuhaim <ka20@windowslive.com>
2019-06-12 23:43:02 +0300

    refactor: add images and .html file to cache. feat: add activate event for service worker.

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'rvw-v0.1';
+const cacheWhiteList = ['rvw-v0.2'];
+const staticCacheName = 'rvw-v0.2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -8,7 +9,20 @@ self.addEventListener('install', (event) => {
         'js/main.js',
         'js/restaurant_info.js',
         'css/styles.css',
-        'img/',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/index.html',
+        '/restaurant.html',
+        '/data/restaurants.json',
+        '/js/dbhelper.js'
       ]);
     })
   );
@@ -18,6 +32,21 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// code from https://developers.google.com/web/fundamentals/primers/service-workers/#update-a-service-worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if(cacheWhiteList.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
